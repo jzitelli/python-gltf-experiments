926bfaf9d92c8d9a69429b2bf21f4013e2e6b471
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-08-08 20:40:38 -0400

    WIP: convert_three.py

--- convert_three.py
+++ convert_three.py
@@ -1,9 +1,23 @@
 import argparse
 import json
+import os
+
+import jsonschema
 
 
+GLTF_SCHEMA_ROOT = os.path.join(os.path.split(__file__)[0], 'schema')
+GLTF_SCHEMAS = {}
+for filename in os.listdir(GLTF_SCHEMA_ROOT):
+    if filename.endswith('.schema.json'):
+        with open(os.path.join(GLTF_SCHEMA_ROOT, filename)) as f:
+            GLTF_SCHEMAS[filename[:-len('.schema.json')]] = json.loads(f.read())
+
+            
 def convert_three(three_json):
-    pass
+    gltf = {prop_name: prop_spec['default']
+            for prop_name, prop_spec in GLTF_SCHEMAS['glTF']['properties'].items()
+            if 'default' in prop_spec}
+    return gltf
 
 
 if __name__ == "__main__":
@@ -14,3 +28,6 @@ if __name__ == "__main__":
     with open(args.input) as f:
         json_str = f.read()
     three_json = json.loads(json_str)
+
+    gltf = convert_three(three_json)
+    
